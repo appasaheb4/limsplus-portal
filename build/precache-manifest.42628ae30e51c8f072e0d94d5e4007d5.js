de80d96457d547d796d88b54d84fa8d61a4eb9bf
onlyappasaheb4 <onlyappasaheb4@gmail.com>
2021-01-08 21:35:40 +0530

    build added

---
+++ build/precache-manifest.42628ae30e51c8f072e0d94d5e4007d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff959a135655632dc5d6027074ac3c11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29013d29f6746b49512e",
+    "url": "/static/css/main.cebfcf30.chunk.css"
+  },
+  {
+    "revision": "80c53e91f83bfa641f82",
+    "url": "/static/js/2.a5bbdcfb.chunk.js"
+  },
+  {
+    "revision": "d3e1ac5415c781e77956b31708ad9d79",
+    "url": "/static/js/2.a5bbdcfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29013d29f6746b49512e",
+    "url": "/static/js/main.24b22b87.chunk.js"
+  },
+  {
+    "revision": "8d38acf959369d0cd0d2",
+    "url": "/static/js/runtime-main.a5953a1f.js"
+  },
+  {
+    "revision": "fa8b0d4a245a4aa320e2713ad2fc53e0",
+    "url": "/static/media/applogo.fa8b0d4a.jpg"
+  }
+]);
