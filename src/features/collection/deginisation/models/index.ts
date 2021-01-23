c6cf01ab0be998cc7387617bc576b8ba51e81ba6
onlyappasaheb4 <onlyappasaheb4@gmail.com>
2021-01-23 13:55:14 +0530

    deginisation

--- src/features/collection/deginisation/models/index.ts
+++ src/features/collection/deginisation/models/index.ts
@@ -2,3 +2,4 @@ export interface IDeginisation {
   code?: string;
   description?: string;
 }
+  
--- src/features/index.ts
+++ src/features/index.ts
@@ -3,11 +3,10 @@ import Banner from "./banner";
 
 // collection
 import Lab from "./collection/labs";
+import Deginisation from "./collection/deginisation";
 
 import Users from "./users";
 import LoginOut from "./login";
 import ChangePassword from "./changePassword";
 
-
-
-export { Dashbord, Banner, Lab, Users, ChangePassword, LoginOut };
+export { Dashbord, Banner, Lab, Deginisation, Users, ChangePassword, LoginOut };
