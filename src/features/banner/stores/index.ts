ba6eaca6a96bffb366eb9187aa93d9757b22e043
onlyappasaheb4 <onlyappasaheb4@gmail.com>
2021-01-24 17:12:36 +0530

    login resposive

--- src/features/banner/stores/index.ts
+++ src/features/banner/stores/index.ts
@@ -6,7 +6,7 @@ import * as Services from "../services";
 @version(0.1)
 class BannerStore {
   @ignore @observable banner?: Models.IBanner;
-  @ignore @observable listBanner: Models.IBanner[] = [];
+  @observable listBanner: Models.IBanner[] = [];
   @action fetchListBanner() {
     Services.listBanner().then((banner) => {
       console.log({ banner });
