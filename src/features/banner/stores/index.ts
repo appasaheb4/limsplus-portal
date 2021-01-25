51e24f9408a6831797543228e13a9056d1ff37d8
onlyappasaheb4 <onlyappasaheb4@gmail.com>
2021-01-25 18:01:33 +0530

    modify diff tsconfig

--- src/features/banner/stores/index.ts
+++ src/features/banner/stores/index.ts
@@ -5,10 +5,10 @@ import * as Services from "../services";
 
 @version(0.1)
 class BannerStore {
-  @ignore @observable banner?: Models.IBanner;
+  @ignore @observable banner?: Models.IBanner;  
   @observable listBanner: Models.IBanner[] = [];
   @action fetchListBanner() {
-    Services.listBanner().then((banner) => {
+    Services.listBanner().then((banner) => {   
       console.log({ banner });
       this.listBanner = banner;
     });
