8c78eb2c5fbf6d344500264ab74a2404e2539b4e
onlyappasaheb4 <onlyappasaheb4@gmail.com>
2021-02-20 12:09:19 +0530

    ip and host change

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-//export const LIMSPLUS_API_HOST = "http://localhost:8080/api"
+//export const LIMSPLUS_API_HOST = "http://02b0c7f31c30.ngrok.io/api"
 export const LIMSPLUS_API_HOST = "https://limsplus-api.azurewebsites.net/api"
