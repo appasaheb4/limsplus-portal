e9a67fae758dd226d3801d14e89973b36734d9fa
appasaheb.lakade <appasaheb.lakade@hiddenbrains.in>
2021-05-23 18:14:31 +0530

    modal file name rename

--- src/library/components/Molecules/index.ts
+++ src/library/components/Molecules/index.ts
@@ -1,9 +1,9 @@
 import * as Modals from "./Modals"
-export { default as ModalConfirm } from "./modal/ModalConfirm"
-export { default as ModalFileUpload } from "./modal/ModalFileUpload"
-export { default as ModalChangePassword } from "./modal/ModalChangePassword"
+export { default as ModalConfirm } from "./modal/ModalConfirm1"
+export { default as ModalFileUpload } from "./modal/ModalFileUpload1"
+export { default as ModalChangePassword } from "./modal/ModalChangePassword1"
 export { default as ModalIdleTimeout } from "./modal/ModalIdleTimeout1"
-export { default as ModalSessionAllowed } from "./modal/ModalSessionAllowed"
+export { default as ModalSessionAllowed } from "./modal/ModalSessionAllowed1"
 export { default as ModalTokenExpire} from './modal/ModalTokenExpire'
 
 export { default as AutocompleteGroupBy } from "./AutocompleteGroupBy"
