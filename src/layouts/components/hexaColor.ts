45d438ec3a3631f887b9af8ff7fc5812b6300e4e
Malav <malavnagar90@gmail.com>
2021-06-25 16:47:01 +0530

    hexaColors

---
+++ src/layouts/components/hexaColor.ts
@@ -0,0 +1,25 @@
+const colorList = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+	'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+];
+
+
+
+
+// const colorList = [
+//     {
+//       color : "#FF6263" 
+//       },
+//     { color: "#DE4839" },
+//     { color: "#BF3325" },
+//     { color: "#E21717" },
+//     { color: "#BF3312" },
+//     { color: "#D82E2F" },
+//     { color: "#EB4D4B" },
+//     { color: "#EF5354" },
+//     { color: "#B4161B" },
+//     { color: "#E6425E" },
+//     { color: "#E83A59" },
+//     { color: "#B9345A" },
+//   ];
+
+export default colorList;
