6310ce1ed1e3190260e60c60c9a774d21bfae5d5
Malav <malavnagar90@gmail.com>
2021-06-25 16:49:26 +0530

    import backGroundImages

---
+++ src/library/assets/backimg.ts
@@ -0,0 +1,6 @@
+import img1 from '../assets/images/back-img-1.jpg'
+import img2 from '../assets/images/back-img-2.jpg'
+import img3 from '../assets/images/back-img-3.jpg'
+import img4 from '../assets/images/back-img-4.jpg'
+
+export {img1,img2,img3,img4};
