66f59cfc0fcb4777c01675a68cf9aa8df97b978c
appasaheb.lakade <appasaheb.lakade@hiddenbrains.in>
2021-06-27 13:15:15 +0530

    code wise seprate record create

--- src/features/collection/testAnalyteMapping/models/index.ts
+++ src/features/collection/testAnalyteMapping/models/index.ts
@@ -1,13 +1,13 @@
 export interface TestAnalyteMapping {
   _id?: string
-  dateCreation?: number  
-  dateActiveFrom?: number  
-  dateActiveTo?: number  
+  dateCreation?: number
+  dateActiveFrom?: number
+  dateActiveTo?: number
   version?: number
   keyNum?: string
-  enteredBy?: string
+  enteredBy?: string  
   lab?: string
-  analyteCode?: string
+  analyteCode?: Array<any>
   testCode?: string
   testName?: string
   description?: string
