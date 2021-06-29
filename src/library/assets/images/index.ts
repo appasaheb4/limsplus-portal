6057679146861080ee9a47c15143753156c892a5
Malav <malavnagar90@gmail.com>
2021-06-29 18:29:03 +0530

    modifed images folder and export images

--- src/library/assets/images/index.ts
+++ src/library/assets/images/index.ts
@@ -1,3 +1,8 @@
+import img from '../images/back-img-1.jpg'
 export const images: { [key: string]: any } = {
-    img1:  "./assets/images/back-img-1.jpg",
-  }
+    img1:  img,
+    // img2: "/back-img-2.jpg",
+    // img3: "/back-img-3.jpg",
+    // img4: "/back-img-4.jpg"
+  
+}
