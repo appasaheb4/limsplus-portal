07685f0a1d6b970b32219f25b6bf9d18cf4a9e27
Malav <malavnagar90@gmail.com>
2021-06-29 18:23:47 +0530

    Remove inline CSS in shortCutBar

--- src/layouts/components/Navbar.tsx
+++ src/layouts/components/Navbar.tsx
@@ -46,7 +46,7 @@ const NavbarComponent = observer(({ dispatch }) => {
           <i className="hamburger align-self-center" />
         </span>
 
-        <Form inline style={{padding:4}}>
+        <Form inline>
           <LibraryComponents.Atoms.Buttons.Button
             size="medium"
             type="outline"
