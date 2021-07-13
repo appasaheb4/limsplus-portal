ae9d21cc9ccf1a85b20f979a66876a21df6269e8
Malav Naagar <malavnagar90@gmail.com>
2021-07-13 15:26:15 +0530

    host change

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-// export const LIMSPLUS_API_HOST = "http://localhost:8080/api"
-=======
 //export const LIMSPLUS_API_HOST = "http://localhost:8080/api"
->>>>>>> 7094785ce57706fbdb5564261851ce173b233bf1
 export const LIMSPLUS_API_HOST = "https://limsplus-api.azurewebsites.net/api"
   
