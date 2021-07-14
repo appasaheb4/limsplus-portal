eba95aa1587fad7ed2bac00c413e3d831288a78e
Malav Naagar <malavnagar90@gmail.com>
2021-07-14 12:51:15 +0530

    add btn version and duplicate

--- src/features/collection/testPanelMapping/scenes/TestPanelMapping.tsx
+++ src/features/collection/testPanelMapping/scenes/TestPanelMapping.tsx
@@ -381,6 +381,24 @@ const TestPanelMapping = observer(() => {
                 body: `Update lab!`,
               })
             }}
+            onVersionUpgrade={(item) => {
+              setModalConfirm({
+                show: true,
+                type: "versionUpgrade",
+                data: item,
+                title: "Are you version upgrade?",
+                body: `Version upgrade this record`,
+              })
+            }}
+            onDuplicate={(item) => {
+              setModalConfirm({
+                show: true,
+                type: "duplicate",
+                data: item,
+                title: "Are you duplicate?",
+                body: `Duplicate this record`,
+              })
+            }}
           />
         </div>
         <LibraryComponents.Molecules.ModalConfirm
@@ -415,7 +433,26 @@ const TestPanelMapping = observer(() => {
                     window.location.reload()
                   }
                 })
+            }else if (type === "versionUpgrade") {
+              Stores.testPanelMappingStore.updateTestPanelMapping({
+                ...modalConfirm.data,
+                _id: undefined,
+                existsVersionId: modalConfirm.data._id,
+                existsRecordId: undefined,
+                version: modalConfirm.data.version + 1,
+                dateActiveFrom: LibraryUtils.moment().unix(),
+              })
+            } else if (type === "duplicate") {
+              Stores.testPanelMappingStore.updateTestPanelMapping({
+                ...modalConfirm.data,
+                _id: undefined,
+                existsVersionId: undefined,
+                existsRecordId: modalConfirm.data._id,
+                version: 1,
+                dateActiveFrom: LibraryUtils.moment().unix(),
+              })
             }
+
           }}
           onClose={() => {
             setModalConfirm({ show: false })
