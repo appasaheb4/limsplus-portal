5908526b7ff978076a380fac731d5448122aa742
Malav Naagar <malavnagar90@gmail.com>
2021-08-10 16:51:35 +0530

    convert model to class Section

---
+++ src/features/collection/section/models/Section.ts
@@ -0,0 +1,24 @@
+export class Section {   
+    departmentCode?: string
+    code?: string
+    name?: string
+    shortName?: string
+    sectionInCharge?: string
+    mobieNo?: string
+    contactNo?: string
+    fyiLine?: string
+    workLine?: string
+    status?: string
+    constructor(rawData: {[key in string]: any}){
+        this.departmentCode = rawData.departmentCode
+        this.code = rawData.code
+        this.name = rawData.name
+        this.shortName = rawData.shortName
+        this.sectionInCharge = rawData.sectionInCharge
+        this.mobieNo = rawData.mobieNo
+        this.contactNo = rawData.fyiLine
+        this.workLine = rawData.workLine
+        this.status = rawData.status
+    }
+  }
+  
