18fb71b68baf6c6a9232b90eb3ccb5c90c80a0ae
Malav Naagar <malavnagar90@gmail.com>
2021-08-10 11:59:22 +0530

    convert model to class Doctor

---
+++ src/features/collection/doctors/models/Doctor.ts
@@ -0,0 +1,97 @@
+export class Doctors {
+    _id?: string
+    existsVersionId?: string
+    existsRecordId?: string
+    dateCreation?: number  
+    dateActiveFrom?: number  
+    dateActiveTo?: number 
+    version?: number
+    keyNum?: string
+    enteredBy?: string
+    doctorCode?: string
+    doctorName?: string
+    sex?: string
+    title?: string
+    firstName?: string
+    middleName?: string
+    lastName?: string
+    reportName?: string
+    address?: string
+    city?: string
+    state?: string
+    country?: string
+    postcode?: number
+    doctorType?: string
+    speciality?: string
+    confidential?: boolean
+    salesTerritoRy?: string
+    area?: string
+    zone?: string
+    telephone?: string
+    mobileNo?: string
+    email?: string
+    workHours?: number
+    deliveryType?: string
+    deliveryMethod?: string
+    edi?: string
+    ediAddress?: string
+    urgent?: boolean
+    registrationLocation?: string
+    lab?: string
+    location?: string
+    schedule?: string
+    reportFormat?: string
+    info?: string
+    fyiLine?: string
+    workLine?: string
+    status?: string
+    constructor(rawData: {[key in string]: any}){
+        this._id = rawData._id
+        this.existsVersionId = rawData.existsVersionId
+        this.existsRecordId = rawData.existsRecordId
+        this.dateCreation = rawData.dateCreation
+        this.dateActiveFrom = rawData.dateActiveFrom
+        this.dateActiveTo = rawData.dateActiveTo
+        this.version = rawData.version
+        this.keyNum = rawData.keyNum
+        this.enteredBy = rawData.enteredBy
+        this.doctorCode = rawData.doctorCode
+        this.doctorName = rawData.doctorName
+        this.sex = rawData.sex
+        this.title = rawData.title
+        this.firstName = rawData.firstName
+        this.middleName = rawData.middleName
+        this.lastName = rawData.lastName
+        this.reportName = rawData.reportName
+        this.address = rawData.address
+        this.city = rawData.city
+        this.state = rawData.state
+        this.country = rawData.country
+        this.postcode = rawData.postcode
+        this.doctorType = rawData.doctorType
+        this.speciality = rawData.speciality
+        this.confidential = rawData.confidential
+        this.salesTerritoRy = rawData.salesTerritoRy
+        this.area = rawData.area
+        this.zone = rawData.zone
+        this.telephone = rawData.telephone
+        this.mobileNo = rawData.mobileNo
+        this.email = rawData.email
+        this.workHours = rawData.workHours
+        this.deliveryType = rawData.deliveryType
+        this.deliveryMethod = rawData.deliveryMethod
+        this.edi = rawData.edi
+        this.ediAddress = rawData.ediAddress
+        this.urgent = rawData.urgent
+        this.registrationLocation = rawData.registrationLocation
+        this.lab = rawData.lab
+        this.location = rawData.location
+        this.schedule = rawData.schedule
+        this.reportFormat = rawData.reportFormat
+        this.info = rawData.info
+        this.fyiLine = rawData.fyiLine
+        this.workLine = rawData.workLine
+        this.status = rawData.status
+    }
+  }
+  
