a8c4db98d4fd1cfd5e75c9b2e89889278d012064
Malav Naagar <malavnagar90@gmail.com>
2021-08-10 16:46:59 +0530

    convert model to class Sample Type

---
+++ src/features/collection/sampleType/models/SampleType.ts
@@ -0,0 +1,15 @@
+export class SampleType {
+    _id?: string
+    sampleCode?: string;
+    sampleType?: string;
+    descriptions?: string;
+    sampleGroup?: string;
+    constructor(rawData: {[key in string]: any}){
+        this._id = rawData._id
+        this.sampleCode = rawData.sampleCode
+        this.sampleType = rawData.sampleType
+        this.descriptions = rawData.descriptions
+        this.sampleGroup = rawData.sampleGroup
+    }
+  }
+  
