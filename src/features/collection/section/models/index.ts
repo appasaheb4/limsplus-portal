cad3a826ae88fed49a731d9b308eeaf1feddf1c0
Malav Naagar <malavnagar90@gmail.com>
2021-08-10 16:51:35 +0530

    convert model to class Section

--- src/features/collection/section/models/index.ts
+++ src/features/collection/section/models/index.ts
@@ -1,12 +1,1 @@
-export interface Section {   
-  departmentCode?: string
-  code?: string
-  name?: string
-  shortName?: string
-  sectionInCharge?: string
-  mobieNo?: string
-  contactNo?: string
-  fyiLine?: string
-  workLine?: string
-  status?: string
-}
+export * from './Section'
