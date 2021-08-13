19a60b44ab735dcf7e783232cb3ff7b908f4e08d
Malav Naagar <malavnagar90@gmail.com>
2021-08-13 13:23:21 +0530

    remove optional

--- src/features/collection/section/models/Section.ts
+++ src/features/collection/section/models/Section.ts
@@ -1,14 +1,14 @@
 export class Section {   
-    departmentCode?: string
-    code?: string
-    name?: string
-    shortName?: string
-    sectionInCharge?: string
-    mobieNo?: string
-    contactNo?: string
-    fyiLine?: string
-    workLine?: string
-    status?: string
+    departmentCode: string
+    code: string
+    name: string
+    shortName: string
+    sectionInCharge: string
+    mobieNo: string
+    contactNo: string
+    fyiLine: string
+    workLine: string
+    status: string
     constructor(rawData: {[key in string]: any}){
         this.departmentCode = rawData.departmentCode
         this.code = rawData.code
@@ -16,7 +16,8 @@ export class Section {
         this.shortName = rawData.shortName
         this.sectionInCharge = rawData.sectionInCharge
         this.mobieNo = rawData.mobieNo
-        this.contactNo = rawData.fyiLine
+        this.contactNo = rawData.contactNo
+        this.fyiLine = rawData.fyiLine
         this.workLine = rawData.workLine
         this.status = rawData.status
     }
