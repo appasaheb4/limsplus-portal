8d0336ad8caa3619d2a2b90e2d842e3bad381720
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-08-25 11:07:46 +0530

    banner count'

--- src/features/collection/labs/services/lab-services.ts
+++ src/features/collection/labs/services/lab-services.ts
@@ -5,18 +5,19 @@
  * @author limsplus
  */
 import * as Models from "../models"
-import { http } from "@lp/library/modules/http"
+import { Http, http, ServiceResponse } from "@lp/library/modules/http"
 
 export class LabService {
   listLabs = (page=0,limit=10) =>
-    new Promise<Models.Labs[]>((resolve, reject) => {
+    new Promise<any>((resolve, reject) => {
       http
         .get(`/master/lab/listlabs/${page}/${limit}`)
-        .then((res: any) => {
-          resolve(res.data.data)
+        .then((response: any) => {
+          const serviceResponse = Http.handleResponse<any>(response)
+          resolve(serviceResponse)
         })
         .catch((error) => {
-          reject({ error })
+          reject(new ServiceResponse<any>(0, error.message, undefined))
         })
     })
   addLab = (lab?: Models.Labs) =>
