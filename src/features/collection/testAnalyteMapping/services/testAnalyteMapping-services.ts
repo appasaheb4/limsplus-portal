74a433a5c3293ac0507d9bc70f63f3348512fa10
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-08-26 15:32:38 +0530

    test analyte mapping:

--- src/features/collection/testAnalyteMapping/services/testAnalyteMapping-services.ts
+++ src/features/collection/testAnalyteMapping/services/testAnalyteMapping-services.ts
@@ -5,18 +5,19 @@
  * @author limsplus
  */
 import * as Models from "../models"
-import { http } from "@lp/library/modules/http"
+import { Http, http, ServiceResponse } from "@lp/library/modules/http"
 
 class TestAnalyteMappingService  {
   listTestAnalyteMapping = (page=0,limit=10) =>
-    new Promise<Models.TestAnalyteMapping[]>((resolve, reject) => {
+    new Promise<any>((resolve, reject) => {
       http
         .get(`master/testAnalyteMapping/listTestAnalyteMapping/${page}/${limit}`)
-        .then((res: any) => {
-          resolve(res.data.data)
+        .then((response: any) => {
+          const serviceResponse = Http.handleResponse<any>(response)
+          resolve(serviceResponse)
         })
         .catch((error) => {
-          reject({ error })
+          reject(new ServiceResponse<any>(0, error.message, undefined))
         })
     })  
   addTestAnalyteMapping = (analyteMapping?: Models.TestAnalyteMapping) =>
