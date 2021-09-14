15e9fe9adde9482ce8ad37f6ea46d4a847dadbd9
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-09-14 18:18:51 +0530

    price list modify

--- src/features/collection/priceList/services/query.ts
+++ src/features/collection/priceList/services/query.ts
@@ -4,11 +4,38 @@ export const GET_PRICELIST = gql`
   query($page: Int, $limit: Int, $env: String, $role: String, $lab: String) {
     getAllPriceList(page: $page, limit: $limit, env: $env, role: $role, lab: $lab) {
       data {
+        _id
         id
+        existsVersionId
+        existsRecordId
+        panelCode
+        panelName
+        priority
+        priceGroup
+        billTo
+        clientName
+        invoiceAc
+        lab
+        price
+        fixedPrice
+        minSp
+        maxSp
+        anyScheme
+        speicalScheme
+        schemePrice
+        disOnScheme
+        enteredBy
+        status
+        environment
+        dateCreation
+        dateActive
+        dateExpiry
+        version
+        keyNum
+        dateOfEntry
+        lastUpdated
       }
       count
     }
   }
 `
-  
-
