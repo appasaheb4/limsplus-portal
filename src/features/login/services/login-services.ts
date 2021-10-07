21f57294128c8e79a9bcad8638e94be9735310d4
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-10-07 11:23:13 +0530

    some user related bug fixed

--- src/features/login/services/login-services.ts
+++ src/features/login/services/login-services.ts
@@ -6,7 +6,7 @@
 
 import { Http, http } from "@lp/library/modules/http"
 import { client, ServiceResponse } from "@lp/library/modules/apolloClient"
-import {LOGIN} from './mutation'
+import {LOGIN,STATUS_UPDATE} from './mutation'
 export class LoginService {
   onLogin = (variables: any) =>
     new Promise<any>((resolve, reject) => {
@@ -22,31 +22,22 @@ export class LoginService {
       .catch((error) =>
         reject(new ServiceResponse<any>(0, error.message, undefined))
       )
-
-      // http
-      //   .post(`/auth/login`, user)
-      //   .then((response) => {
-      //     const serviceResponse = Http.handleResponse<any>(response)
-      //     resolve(serviceResponse)
-      //   })
-      //   .catch((error) => {
-      //     reject(new ServiceResponse<any>(0, error.message, undefined))
-      //   })
-
-
     })
 
-  accountStatusUpdate = (statusInfo: any) =>
+  accountStatusUpdate = (variables: any) =>
     new Promise<any>((resolve, reject) => {
-      http
-        .post(`auth/statusUpdate`, statusInfo)
-        .then((response) => {
-          const serviceResponse = Http.handleResponse<any>(response)
-          resolve(serviceResponse)
-        })
-        .catch((error) => {
-          reject(new ServiceResponse<any>(0, error.message, undefined))
-        })
+      client  
+      .mutate({
+        mutation: STATUS_UPDATE,
+        variables
+      })
+      .then((response: any) => {
+        console.log({ response })
+        resolve(response.data)
+      })
+      .catch((error) =>
+        reject(new ServiceResponse<any>(0, error.message, undefined))
+      )
     })
   forgotPassword = (userInfo: any) =>
     new Promise<any>((resolve, reject) => {
