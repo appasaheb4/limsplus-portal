36ccc2a70b5d6fc03fc8cd49662fa6ae415ac0db
Malav Naagar <malavnagar90@gmail.com>
2021-10-15 12:49:24 +0530

    pending add state multiple

--- src/features/collection/administrativeDivisions/stores/administrativeDivisions-store.ts
+++ src/features/collection/administrativeDivisions/stores/administrativeDivisions-store.ts
@@ -2,13 +2,15 @@ import { version, ignore } from "mobx-sync"
 import { makeAutoObservable, action, observable, computed } from "mobx"
 import * as Models from "../models"
 import * as Services from "../services"
-
+interface LocalState {
+   state: string
+}
 @version(0.1)
 class AdministrativeDivisionsStore {
   @ignore @observable administrativeDiv?: Models.AdministrativeDivisions
   @observable listAdministrativeDivCount: number = 0
   @observable listAdministrativeDiv?: Models.AdministrativeDivisions[] = []
-
+  @ignore @observable  localState!: LocalState 
   constructor() {
     makeAutoObservable(this)
   }
@@ -31,6 +33,9 @@ class AdministrativeDivisionsStore {
 
   @action updateAdministrativeDiv(administrative: Models.AdministrativeDivisions) {
     this.administrativeDiv = administrative
+  }
+  @action updateLoclaState(state: LocalState){
+    this.localState = state
   }
 }
 
