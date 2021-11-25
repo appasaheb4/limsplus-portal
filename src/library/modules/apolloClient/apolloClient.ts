a81f8eb16f38438ec34f02bd858decb36c5c40a0
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-11-25 13:45:56 +0530

    some changes

--- src/library/modules/apolloClient/apolloClient.ts
+++ src/library/modules/apolloClient/apolloClient.ts
@@ -26,7 +26,7 @@ const authLink = setContext(async (_, { headers }) => {
   }
 })
         
-// depoly 2  
+// depoly 1 
 const UploadLink = createUploadLink({
   //uri: "http://localhost:8080/graphql",
   //uri: "https://f6e6-2409-4042-4cca-1e28-c5f5-a35d-4618-b21e.ngrok.io/graphql",
