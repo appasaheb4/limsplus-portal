cb9902e327f7e5011f3bd3dcb90faa84aac10a49
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-11-28 21:50:57 +0530

    date filter

--- src/library/components/Organisms/utils.ts
+++ src/library/components/Organisms/utils.ts
@@ -1,4 +1,4 @@
-import { textFilter,customFilter } from "react-bootstrap-table2-filter"
+import { textFilter,customFilter,dateFilter,Comparator } from "react-bootstrap-table2-filter"
 import { Type } from "react-bootstrap-table2-editor"
   
-export { textFilter,customFilter,Type }
+export { textFilter,customFilter,Type,dateFilter,Comparator }
