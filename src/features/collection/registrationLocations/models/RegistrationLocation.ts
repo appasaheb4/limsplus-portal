a50a3fa1369167e04713de4558ffa1cd0881a68c
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-12-24 13:43:34 +0530

    registration location modify

--- src/features/collection/registrationLocations/models/RegistrationLocation.ts
+++ src/features/collection/registrationLocations/models/RegistrationLocation.ts
@@ -1,104 +1,107 @@
 export class RegistrationLocations {
-    _id: string
-    existsVersionId: string
-    existsRecordId: string
-    dateCreation: Date
-    dateActive: Date
-    dateExpire: Date
-    version: number
-    enteredBy: string
-    locationCode: string
-    locationName: string
-    address: string
-    city: string
-    state: string
-    country: string
-    postcode: number
-    customerGroup: string
-    category: string
-    confidential: boolean
-    telephone: string
-    mobileNo: string
-    email: string
-    deliveryType: string
-    deliveryMethod: string
-    corporateCode: string
-    invoiceAc: string
-    labLicence: string
-    printLabel: boolean
-    methodColn: string
-    workHrs: number
-    salesTerritoRy: string
-    area: string
-    zone: string
-    route: string
-    lab: string
-    location: string
-    neverBill: boolean
-    edi: string
-    ediAddress: string
-    urgent: boolean
-    schedule: string
-    reportFormat: string
-    info: string
-    fyiLine: string
-    workLine: string
-    status: string
-    environment: string
-    dateOfEntry: Date
-    lastUpdated: Date
+  _id: string
+  existsVersionId: string
+  existsRecordId: string
+  dateCreation: Date
+  dateActive: Date
+  dateExpire: Date
+  version: number
+  enteredBy: string
+  locationCode: string
+  locationName: string
+  address: string
+  city: string
+  state: string
+  country: string
+  postcode: number
+  customerGroup: string
+  category: string
+  confidential: boolean
+  telephone: string
+  mobileNo: string
+  email: string
+  deliveryType: string
+  deliveryMethod: string
+  corporateCode: string
+  invoiceAc: string
+  labLicence: string
+  printLabel: boolean
+  methodColn: string
+  workHrs: number
+  salesTerritoRy: string
+  area: string
+  zone: string
+  route: string
+  lab: string
+  location: string
+  neverBill: boolean
+  edi: string
+  ediAddress: string
+  urgent: boolean
+  schedule: string
+  reportFormat: string
+  info: string
+  fyiLine: string
+  workLine: string
+  acClass: string
+  accountType: string
+  status: string
+  environment: string
+  dateOfEntry: Date
+  lastUpdated: Date
 
-    constructor(rawData: {[key in string]: any}){
-      this._id = rawData._id
-      this.existsVersionId = rawData.existsVersionId
-      this.existsRecordId = rawData.existsRecordId
-      this.dateCreation = rawData.dateCreation
-      this.dateActive = rawData.dateActive
-      this.dateExpire = rawData.dateExpire
-      this.version = rawData.version
-      this.enteredBy = rawData.enteredBy
-      this.locationCode = rawData.locationCode
-      this.locationName = rawData.locationName
-      this.address = rawData.address
-      this.city = rawData.city
-      this.state = rawData.state
-      this.country = rawData.country
-      this.postcode = rawData.postcode
-      this.customerGroup = rawData.customerGroup
-      this.category = rawData.category
-      this.confidential = rawData.confidential
-      this.schedule = rawData.schedule
-      this.telephone = rawData.telephone
-      this.mobileNo = rawData.mobileNo
-      this.email = rawData.email
-      this.deliveryType = rawData.deliveryType
-      this.deliveryMethod = rawData.deliveryMethod
-      this.urgent = rawData.urgent
-      this.corporateCode = rawData.corporateCode
-      this.invoiceAc = rawData.invoiceAc
-      this.labLicence = rawData.labLicence
-      this.printLabel = rawData.printLabel
-      this.methodColn = rawData.methodColn
-      this.workHrs = rawData.workHrs
-      this.salesTerritoRy = rawData.salesTerritoRy
-      this.area = rawData.area
-      this.zone = rawData.zone
-      this.route = rawData.route
-      this.lab = rawData.lab
-      this.location = rawData.location
-      this.neverBill = rawData.neverBill
-      this.edi = rawData.edi
-      this.printLabel = rawData.printLabel
-      this.ediAddress = rawData.ediAddress
-      this.schedule = rawData.schedule
-      this.reportFormat = rawData.reportFormat
-      this.info = rawData.info
-      this.fyiLine = rawData.fyiLine
-      this.workLine = rawData.workLine
-      this.status = rawData.status
-      this.environment = rawData.environment
-      this.dateOfEntry = rawData.dateOfEntry
-      this.lastUpdated = rawData.lastUpdated
-    }
+  constructor(rawData: { [key in string]: any }) {
+    this._id = rawData._id
+    this.existsVersionId = rawData.existsVersionId
+    this.existsRecordId = rawData.existsRecordId
+    this.dateCreation = rawData.dateCreation
+    this.dateActive = rawData.dateActive
+    this.dateExpire = rawData.dateExpire
+    this.version = rawData.version
+    this.enteredBy = rawData.enteredBy
+    this.locationCode = rawData.locationCode
+    this.locationName = rawData.locationName
+    this.address = rawData.address
+    this.city = rawData.city
+    this.state = rawData.state
+    this.country = rawData.country
+    this.postcode = rawData.postcode
+    this.customerGroup = rawData.customerGroup
+    this.category = rawData.category
+    this.confidential = rawData.confidential
+    this.schedule = rawData.schedule
+    this.telephone = rawData.telephone
+    this.mobileNo = rawData.mobileNo
+    this.email = rawData.email
+    this.deliveryType = rawData.deliveryType
+    this.deliveryMethod = rawData.deliveryMethod
+    this.urgent = rawData.urgent
+    this.corporateCode = rawData.corporateCode
+    this.invoiceAc = rawData.invoiceAc
+    this.labLicence = rawData.labLicence
+    this.printLabel = rawData.printLabel
+    this.methodColn = rawData.methodColn
+    this.workHrs = rawData.workHrs
+    this.salesTerritoRy = rawData.salesTerritoRy
+    this.area = rawData.area
+    this.zone = rawData.zone
+    this.route = rawData.route
+    this.lab = rawData.lab
+    this.location = rawData.location
+    this.neverBill = rawData.neverBill
+    this.edi = rawData.edi
+    this.printLabel = rawData.printLabel
+    this.ediAddress = rawData.ediAddress
+    this.schedule = rawData.schedule
+    this.reportFormat = rawData.reportFormat
+    this.info = rawData.info
+    this.fyiLine = rawData.fyiLine
+    this.workLine = rawData.workLine
+    this.acClass = rawData.acClass
+    this.accountType = rawData.accountType   
+    this.status = rawData.status
+    this.environment = rawData.environment
+    this.dateOfEntry = rawData.dateOfEntry
+    this.lastUpdated = rawData.lastUpdated
   }
-  
+}
