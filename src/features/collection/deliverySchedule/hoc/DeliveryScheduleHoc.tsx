d78a690537eaa3745527fe168830d4085ec35843
Malav Naagar <malavnagar90@gmail.com>
2021-12-27 14:36:06 +0530

    delivery schedule hoc fix

---
+++ src/features/collection/deliverySchedule/hoc/DeliveryScheduleHoc.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable */
+import React, { useEffect } from "react"
+import { observer } from "mobx-react"
+import { useStores } from "@lp/stores"
+import * as LibraryUtils from "@lp/library/utils"
+
+export const DeliveryScheduleHoc = (Component: React.FC<any>) => {
+  return observer(
+    (props: any): JSX.Element => {
+      const { loginStore, deliveryScheduleStore, routerStore } = useStores()
+      useEffect(() => {
+        if (loginStore.login && loginStore.login.role !== "SYSADMIN") {
+          deliveryScheduleStore.updateDeliverySchedule({
+            ...deliveryScheduleStore.deliverySchedule,
+            environment: loginStore.login.environment,
+          })
+        }
+        deliveryScheduleStore.updateDeliverySchedule({
+          ...deliveryScheduleStore.deliverySchedule,
+          environment: LibraryUtils.getDefaultLookupItem(
+            routerStore.lookupItems,
+            "ENVIRONMENT"
+          ),
+        })
+        deliveryScheduleStore.updateDeliverySchedule({
+          ...deliveryScheduleStore.deliverySchedule,
+          processingType: LibraryUtils.getDefaultLookupItem(
+            routerStore.lookupItems,
+            "PROCESSING_TYPE"
+          ),
+        })
+        deliveryScheduleStore.updateDeliverySchedule({
+          ...deliveryScheduleStore.deliverySchedule,
+          dynamicTU: LibraryUtils.getDefaultLookupItem(
+            routerStore.lookupItems,
+            "DYNAMIC_TU"
+          ),
+        })
+      }, [loginStore.login, routerStore.lookupItems])
+
+      return <Component {...props} />
+    }
+  )
+}
