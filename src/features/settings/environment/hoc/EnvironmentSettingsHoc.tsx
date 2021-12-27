94bd71cb6067964ac1b65c4fb9e4780e9d83f839
Malav Naagar <malavnagar90@gmail.com>
2021-12-27 17:06:12 +0530

    environment setting hoc fix

---
+++ src/features/settings/environment/hoc/EnvironmentSettingsHoc.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import React, { useEffect } from "react"
+import { observer } from "mobx-react"
+import { useStores } from "@lp/stores"
+import * as LibraryUtils from "@lp/library/utils"
+
+export const EnvironmentSettingsHoc = (Component: React.FC<any>) => {
+  return observer(
+    (props: any): JSX.Element => {
+      const { loginStore, environmentStore, routerStore } = useStores()
+      useEffect(() => {
+        if (loginStore.login && loginStore.login.role !== "SYSADMIN") {
+          environmentStore.updateEnvironmentSettings({
+            ...environmentStore.environmentSettings,
+            environment: loginStore.login.environment,
+          })
+        }
+        environmentStore.updateEnvironmentSettings({
+          ...environmentStore.environmentSettings,
+          environment: LibraryUtils.getDefaultLookupItem(
+            routerStore.lookupItems,
+            "SESSION_ENVIRONMENT"
+          ),
+        })
+      }, [loginStore.login, routerStore.lookupItems])
+
+      return <Component {...props} />
+    }
+  )
+}
