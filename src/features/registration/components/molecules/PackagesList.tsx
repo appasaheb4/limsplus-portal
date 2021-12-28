d23c8f320eb88444c68755765e0dbaa6640e01a7
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-12-28 22:33:37 +0530

    service type m

--- src/features/registration/components/molecules/PackagesList.tsx
+++ src/features/registration/components/molecules/PackagesList.tsx
@@ -8,7 +8,7 @@ interface PackagesListProps {
 export const PackagesList = ({ data }: PackagesListProps) => {
   return (
     <>
-      {data?.pacakgeListS && (
+      {data?.pacakgeListS && data?.pacakgeListS?.length > 0 && (
         <>
           <h4 className="text-center"> Service Type: S</h4>
           <Table striped bordered>
@@ -23,6 +23,33 @@ export const PackagesList = ({ data }: PackagesListProps) => {
             </thead>
             <tbody className="text-xs">
               {data?.pacakgeListS?.map((item) => (
+                <tr key={item.panelCode}>
+                  <td>{item?.panelCode}</td>
+                  <td>{item?.panelName}</td>
+                  <td>{item?.packageCode}</td>
+                  <td>{item.serviceType}</td>
+                  <td>Delete</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+      {data?.pacakgeListM && data?.pacakgeListM?.length > 0 && (
+        <>
+          <h4 className="text-center"> Service Type: M</h4>
+          <Table striped bordered>
+            <thead>
+              <tr className="p-0 text-xs">
+                <th className="text-white">Panel Code</th>
+                <th className="text-white">Panel Name</th>
+                <th className="text-white">Package</th>
+                <th className="text-white">Service Type</th>
+                <th className="text-white">Operation</th>
+              </tr>
+            </thead>
+            <tbody className="text-xs">
+              {data?.pacakgeListM?.map((item) => (
                 <tr key={item.panelCode}>
                   <td>{item?.panelCode}</td>
                   <td>{item?.panelName}</td>
