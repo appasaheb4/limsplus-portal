b227578b8df3325969e00c7db50635e5fcd84a0d
appasaheb.lakade <onlyappasaheb4@gmail.com>
2021-12-29 15:09:42 +0530

    firebase added

---
+++ firebase.ts
@@ -0,0 +1,23 @@
+//npm install firebase
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBSEVfJAzr5qYLFmpF3yZQUp-bm56YtXbE",
+//   authDomain: "lims-plus-854e4.firebaseapp.com",
+//   projectId: "lims-plus-854e4",
+//   storageBucket: "lims-plus-854e4.appspot.com",
+//   messagingSenderId: "723104013699",
+//   appId: "1:723104013699:web:52a9edfd4c898101216ab6",
+//   measurementId: "G-46WKN1KLQM"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
