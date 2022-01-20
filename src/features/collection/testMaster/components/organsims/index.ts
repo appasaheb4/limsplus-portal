4f5ab14276c0e7fb19d80f3fa68b41aecfbe0f75
Malav Naagar <malavnagar90@gmail.com>
2022-01-20 14:46:37 +0530

    panel master and test master ui bug

--- src/features/collection/testMaster/components/organsims/index.ts
+++ src/features/collection/testMaster/components/organsims/index.ts
@@ -1,3 +1,4 @@
 export * from './AutoCompleteFilterSingleSelectLabs'
 export * from './AutoCompleteFilterSingleSelectDepartment'
 export * from './AutoCompleteFilterSingleSelectDeliverySchedule'
+export * from "./AutoCompleteFilterSingleSelectTestMethod"
