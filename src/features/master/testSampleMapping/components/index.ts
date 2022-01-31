4746e9cc481296822f6fe508ac4d83dcdb790247
Malav <malavnagar90@gmail.com>
2022-01-31 16:05:45 +0530

    test sample mapping code refactor

--- src/features/master/testSampleMapping/components/index.ts
+++ src/features/master/testSampleMapping/components/index.ts
@@ -1,3 +1,8 @@
-import * as Molecules from "./molecules"
-import * as Orgransims from "./orgransims"  
-export { Molecules,Orgransims }  
+export * from "./molecules/TestSampleMappingList"
+export * from "./orgransims/AutoCompleteFilterSingleSelectContainerCode"
+export * from "./orgransims/AutoCompleteFilterSingleSelectContainerName"
+export * from "./orgransims/AutoCompleteFilterSingleSelectDepartment"
+export * from "./orgransims/AutoCompleteFilterSingleSelectSampleCode"
+export * from "./orgransims/AutoCompleteFilterSingleSelectSampleGroup"
+export * from "./orgransims/AutoCompleteFilterSingleSelectSampleType"
+export * from "./orgransims/AutoCompleteFilterSingleSelectTestCode"
