b99239d33a342e9d5ecfb96e9f63ded6e1b6dc7d
Malav <malavnagar90@gmail.com>
2022-02-02 14:13:52 +0530

    price code refactor fix

--- src/features/master/priceList/components/organsims/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AutoCompleteFilterSingleSelectLabs'
-export * from './AutoCompleteFilterSingleSelectBillTo'
-export * from './AutoCompleteFilterSingleSelectCorprateName'
-export * from './AutoCompleteFilterSingleSelectInvoiceAc'
-export * from './AutoCompleteFilterSingleSelectPanelCode'
-export * from './AutoCompleteFilterSingleSelectPanelName'
