edaf869e21b679893fb25b8c31ac447bb40b7f22
appasaheb4 <onlyappasaheb4@gmail.com>
2022-04-09 20:43:31 +0530

    stories created for button

--- src/library/components/atoms/buttons/button.stories.tsx
+++ src/library/components/atoms/buttons/button.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
-import {Button} from './button.component';
+import {Button, ButtonCircleAddRemove} from './button.component';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Atoms/Button',
+  title: 'Atoms/Buttons',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
@@ -12,6 +12,13 @@ export default {
   },
 } as ComponentMeta<typeof Button>;
 
-export const Primary: ComponentStory<typeof Button> = () => (
+export const _Button: ComponentStory<typeof Button> = () => (
   <Button onClick={() => {}}>LimsPlus</Button>
+);
+
+export const _ButtonCircleAddRemove: ComponentStory<typeof Button> = () => (
+  <ButtonCircleAddRemove
+    show={true}
+    onClick={status => console.log({status})}
+  />
 );
