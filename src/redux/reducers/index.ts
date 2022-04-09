c15d250df941378a5af61230c2c77ec9278ad767
appasaheb4 <onlyappasaheb4@gmail.com>
2022-04-09 19:53:24 +0530

    eslint bug fixed.

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,14 +1,15 @@
-import { combineReducers } from "redux";
+/* eslint-disable */
+import {combineReducers} from 'redux';
 
-import sidebar from "./sidebarReducers";
-import layout from "./layoutReducer";
-import theme from "./themeReducer";
+import sidebar from './sidebarReducers';
+import layout from './layoutReducer';
+import theme from './themeReducer';
 
-import { reducer as toastr } from "react-redux-toastr";
+import {reducer as toastr} from 'react-redux-toastr';
 
 export default combineReducers({
   sidebar,
   layout,
   theme,
-  toastr
+  toastr,
 });
--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -1,30 +1,31 @@
-import { action, computed, makeObservable, observable } from "mobx"
-import _ from "lodash"
+/* eslint-disable */
+import {action, computed, makeObservable, observable} from 'mobx';
+import _ from 'lodash';
 
 export class Store {
-  private isLoading: boolean
-  flagLoading: boolean
+  private isLoading: boolean;
+  flagLoading: boolean;
 
   constructor() {
-    this.isLoading = false
-    this.flagLoading = true
+    this.isLoading = false;
+    this.flagLoading = true;
     makeObservable<Store, any>(this, {
       isLoading: observable,
       flagLoading: observable,
       loading: computed,
       setLoading: action,
-    })
+    });
   }
 
   get loading(): boolean {
-    return this.isLoading
+    return this.isLoading;
   }
 
   setLoading(flag: boolean): void {
-    this.isLoading = _.isNil(flag) || flag
+    this.isLoading = _.isNil(flag) || flag;
   }
 
   uploadLoadingFlag(loading: boolean) {
-    this.flagLoading = loading
+    this.flagLoading = loading;
   }
 }
