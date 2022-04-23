bbef1a6b9fe962ebc44f0afd8cb502f2a12491a1
Malav <malavnagar90@gmail.com>
2022-04-23 19:48:50 +0530

    test case fix

--- src/library/components/atoms/grid/grid.component.test.tsx
+++ src/library/components/atoms/grid/grid.component.test.tsx
@@ -1,1 +1,8 @@
-export {};
+import React from 'react';
+import {render} from '@utils';
+import {Grid} from './grid.component';
+
+it('render grid correctly', () => {
+  const grid = render(<Grid cols={3} />);
+  expect(grid).toMatchSnapshot();
+});
