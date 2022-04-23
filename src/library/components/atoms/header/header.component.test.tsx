bbf75d70f395dac70c7fb984328d7190cdf8a677
Malav <malavnagar90@gmail.com>
2022-04-23 19:48:50 +0530

    test case fix

--- src/library/components/atoms/header/header.component.test.tsx
+++ src/library/components/atoms/header/header.component.test.tsx
@@ -1,1 +1,17 @@
-export {};
+import React from 'react';
+import {render} from '@utils';
+import {Header, PageHeading, PageHeadingLabDetails} from './header.component';
+
+it('render header correctly', () => {
+  const header = render(<Header> Im Heading</Header>);
+  expect(header).toMatchSnapshot();
+});
+
+it('render pageHeading correctly', () => {
+  const pageHeading = render(<PageHeading title="Im Page Title" />);
+  expect(pageHeading).toMatchSnapshot();
+});
+it('render pageHeadingLabDetails correctly', () => {
+  const pageHeadingLabDetails = render(<PageHeadingLabDetails store={''} />);
+  expect(pageHeadingLabDetails).toMatchSnapshot();
+});
--- src/library/components/atoms/image/image.component.test.tsx
+++ src/library/components/atoms/image/image.component.test.tsx
@@ -1,1 +1,14 @@
-export {};
+import React from 'react';
+import {render} from '@utils';
+import {Image, ImageBackground} from './image.component';
+
+it('render image correctly', () => {
+  const image = render(<Image source="" onClick={() => jest.fn()} />);
+  expect(image).toMatchSnapshot();
+});
+it('render imageBackground correctly', () => {
+  const imageBackground = render(
+    <ImageBackground source="" position="center" fit="cover" />,
+  );
+  expect(imageBackground).toMatchSnapshot();
+});
