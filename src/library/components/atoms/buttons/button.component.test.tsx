5118ff86c1f7a122cf8bb5ad131191b52be56f48
appasaheb4 <onlyappasaheb4@gmail.com>
2022-05-19 10:59:00 +0530

    patient result modify

--- src/library/components/atoms/buttons/button.component.test.tsx
+++ src/library/components/atoms/buttons/button.component.test.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import {render} from '@utils';
-import {
-  Button,
-  ButtonCircleAddRemove,
-  ButtonCircleAddRemoveBottom,
-} from './button.component';
+import {Button} from './button.component';
 
 it('render button correctly', () => {
   const button = render(
@@ -15,16 +11,16 @@ it('render button correctly', () => {
   expect(button).toMatchSnapshot();
 });
 
-it('render buttonCircleAddRemove correctly ', () => {
-  const buttonCircleAddRemove = render(
-    <ButtonCircleAddRemove show={true} onClick={() => jest.fn()} />,
-  );
-  expect(buttonCircleAddRemove).toMatchSnapshot();
-});
+// it('render buttonCircleAddRemove correctly ', () => {
+//   const buttonCircleAddRemove = render(
+//     <ButtonCircleAddRemove show={true} onClick={() => jest.fn()} />,
+//   );
+//   expect(buttonCircleAddRemove).toMatchSnapshot();
+// });
 
-it('render buttonCircleAddRemoveBottom correctly ', () => {
-  const buttonCircleAddRemoveBottom = render(
-    <ButtonCircleAddRemoveBottom show={true} onClick={() => jest.fn()} />,
-  );
-  expect(buttonCircleAddRemoveBottom).toMatchSnapshot();
-});
+// it('render buttonCircleAddRemoveBottom correctly ', () => {
+//   const buttonCircleAddRemoveBottom = render(
+//     <ButtonCircleAddRemoveBottom show={true} onClick={() => jest.fn()} />,
+//   );
+//   expect(buttonCircleAddRemoveBottom).toMatchSnapshot();
+// });
