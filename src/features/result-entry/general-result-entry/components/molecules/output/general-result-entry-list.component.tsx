e667564f0ea25cad148d71f05d0d2b1b70766db3
appasaheb4 <onlyappasaheb4@gmail.com>
2022-06-01 15:58:38 +0530

    patient result test status update

--- src/features/result-entry/general-result-entry/components/molecules/output/general-result-entry-list.component.tsx
+++ src/features/result-entry/general-result-entry/components/molecules/output/general-result-entry-list.component.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-case-declarations */
 import React from 'react';
 import dayjs from 'dayjs';
+import _ from 'lodash';
 import {lookupItems, lookupValue} from '@/library/utils';
 import {
   NumberFilter,
@@ -13,6 +15,7 @@ import {
   Buttons,
 } from '@/library/components';
 import {InputResult} from './input-result.components';
+import {DisplayResult} from './display-result.components';
 
 let analyteCode;
 let analyteName;
@@ -29,7 +32,6 @@ let low;
 let high;
 let alpha;
 let enteredBy;
-let status;
 let environment;
 let dateCreation;
 let dateActive;
@@ -59,6 +61,84 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
   const editorCell = (row: any) => {
     return row.status !== 'I' ? true : false;
   };
+
+  const getStatus = (
+    status: string,
+    type: string,
+    result: number,
+    lo: number,
+    hi: number,
+  ) => {
+    if (status === 'resultStatus' && type === 'V') {
+      if (result >= lo && result <= hi) return 'N';
+      else if (result < lo) return 'L';
+      else if (result > hi) return 'H';
+      return 'N';
+    } else if (status === 'testStatus' && type === 'V') {
+      if (result >= lo && result <= hi) return 'N';
+      else if (result < lo) return 'A';
+      else if (result > hi) return 'A';
+      return 'N';
+    }
+    return 'N';
+  };
+
+  const getResultStatus = (type: string, row: any) => {
+    switch (type) {
+      case 'V':
+        if (!row?.loNor && !row?.hiNor) return 'N';
+        const numberResult = Number.parseFloat(
+          row?.result?.replace(/[^0-9]/g, ''),
+        );
+        const numberLo = Number.parseFloat(
+          row?.loNor?.replace(/[^0-9]/g, '') || 0,
+        );
+        const numberHi = Number.parseFloat(
+          row?.hiNor?.replace(/[^0-9]/g, '') || 0,
+        );
+        return getStatus(
+          'resultStatus',
+          type,
+          numberResult,
+          numberLo,
+          numberHi,
+        );
+        break;
+      case 'D' || 'L' || 'F' || 'M':
+        return row?.abnFlag ? 'A' : 'N';
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const getTestStatus = (type: string, row: any) => {
+    switch (type) {
+      case 'V':
+        if (!row?.loNor && !row?.hiNor) return 'N';
+        // eslint-disable-next-line no-case-declarations
+        const numberResult = Number.parseFloat(
+          row?.result?.replace(/[^0-9]/g, ''),
+        );
+        const numberLo = Number.parseFloat(
+          row?.loNor?.replace(/[^0-9]/g, '') || 0,
+        );
+        const numberHi = Number.parseFloat(
+          row?.hiNor?.replace(/[^0-9]/g, '') || 0,
+        );
+        return getStatus('testStatus', type, numberResult, numberLo, numberHi);
+        break;
+
+      case 'D' || 'L' || 'F' || 'M':
+        return row?.abnFlag ? 'A' : 'N';
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       <div style={{position: 'relative'}}>
@@ -77,6 +157,11 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
               dataField: 'result',
               text: 'Result',
               headerClasses: 'textHeader',
+              formatter: (cellContent, row) => (
+                <>
+                  <DisplayResult row={row} />
+                </>
+              ),
               editorRenderer: (
                 editorProps,
                 value,
@@ -87,9 +172,9 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
               ) => (
                 <>
                   <InputResult
-                    resultType={row.resultType}
+                    row={row}
                     onSelect={result => {
-                      props.onUpdateValue({result}, row._id);
+                      props.onUpdateValue(result, row._id);
                     }}
                   />
                 </>
@@ -140,8 +225,7 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
                       disabled={!editorCell(row)}
                       value={row.reportable}
                       onChange={reportable => {
-                        // props.onUpdateItem &&
-                        //   props.onUpdateItem(method, 'method', row._id);
+                        props.onUpdateValue({reportable}, row._id);
                       }}
                     />
                   </>
@@ -169,11 +253,14 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
                 return (
                   <>
                     <Form.Toggle
-                      disabled={true}
+                      disabled={
+                        row.resultType === 'F' || row.resultType === 'M'
+                          ? false
+                          : true
+                      }
                       value={row.abnFlag}
                       onChange={abnFlag => {
-                        // props.onUpdateItem &&
-                        //   props.onUpdateItem(method, 'method', row._id);
+                        props.onUpdateValue({abnFlag}, row._id);
                       }}
                     />
                   </>
@@ -188,11 +275,14 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
                 return (
                   <>
                     <Form.Toggle
-                      disabled={true}
+                      disabled={
+                        row.resultType === 'F' || row.resultType === 'M'
+                          ? false
+                          : true
+                      }
                       value={row.critical}
                       onChange={critical => {
-                        // props.onUpdateItem &&
-                        //   props.onUpdateItem(method, 'method', row._id);
+                        props.onUpdateValue({critical}, row._id);
                       }}
                     />
                   </>
@@ -210,8 +300,7 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
                       disabled={!editorCell(row)}
                       value={row.showRanges}
                       onChange={showRanges => {
-                        // props.onUpdateItem &&
-                        //   props.onUpdateItem(method, 'method', row._id);
+                        props.onUpdateValue({showRanges}, row._id);
                       }}
                     />
                   </>
@@ -251,7 +340,24 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
             {
               dataField: 'conclusion',
               text: 'Conclusion',
-              editable: false,
+              headerClasses: 'textHeader',
+              editorRenderer: (
+                editorProps,
+                value,
+                row,
+                column,
+                rowIndex,
+                columnIndex,
+              ) => (
+                <>
+                  <Form.Input
+                    placeholder='Conclusion'
+                    onBlur={conclusion => {
+                      props.onUpdateValue({conclusion}, row._id);
+                    }}
+                  />
+                </>
+              ),
             },
             {
               dataField: 'testStatus',
@@ -280,16 +386,31 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
               formatter: (cellContent, row) => (
                 <>
                   <div className='flex flex-row'>
-                    {row.testStatus === 'P' && row.resultStatus === 'P' && (
+                    {row.testStatus === 'P' && (
                       <>
                         <Buttons.Button
                           size='small'
                           type='outline'
                           buttonClass='text-white'
                           onClick={() => {
+                            if (!row?.result)
+                              return alert('Please enter result value ');
                             // eslint-disable-next-line @typescript-eslint/no-unused-expressions
                             props.onSaveFields &&
-                              props.onSaveFields(row, row._id);
+                              props.onSaveFields(
+                                {
+                                  ...row,
+                                  resultStatus: getResultStatus(
+                                    row.resultType,
+                                    row,
+                                  ),
+                                  testStatus: getTestStatus(
+                                    row.resultType,
+                                    row,
+                                  ),
+                                },
+                                row._id,
+                              );
                           }}
                         >
                           {'Save'}
@@ -332,7 +453,6 @@ export const GeneralResultEntryList = (props: GeneralResultEntryListProps) => {
             high('');
             alpha('');
             enteredBy('');
-            status('');
             environment('');
             dateCreation();
             dateActive();
