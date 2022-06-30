3a7a65d6dba24219f2d1fec972eb8b43e39abb93
appasaheb4 <onlyappasaheb4@gmail.com>
2022-06-30 11:21:29 +0530

    font setting modify

--- src/features/report-builder/report-settings/models/index.ts
+++ src/features/report-builder/report-settings/models/index.ts
@@ -2,3 +2,4 @@ export * from './report-section.model';
 export * from './section-setting.model';
 export * from './page-setting.model';
 export * from './general-setting.model';
+export * from './font-setting.model';
