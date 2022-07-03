c5830e042c5e5428d1ebb717be1ac83f2757e1b1
appasaheb4 <onlyappasaheb4@gmail.com>
2022-07-03 22:08:37 +0530

    font page update

--- src/features/report-builder/report-settings/services/index.ts
+++ src/features/report-builder/report-settings/services/index.ts
@@ -2,3 +2,4 @@ export * from './report-section.service';
 export * from './section-setting.service';
 export * from './page-setting.service';
 export * from './general-setting.service';
+export * from './font-setting.service';
