1669b4f67fc4ffb89db2087eb137a6d19e543468
Malav Naagar <73930050+malav4all@users.noreply.github.com>
2022-08-08 17:20:55 +0530

    patient-registration test file

---
+++ src/features/registration/screens/patient-registration/patient-manager.screen.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import {PatientManager} from '..';
+
+describe('PatientManager Screen', () => {
+  it('render patientManager correctly', () => {
+    const patientManager = render(<PatientManager />);
+    expect(patientManager).toMatchSnapshot();
+  });
+});
