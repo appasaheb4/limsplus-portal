1ddb4e43d6b5caff6707dd76c8fecaa32adc6988
Malav Naagar <73930050+malav4all@users.noreply.github.com>
2022-08-08 17:20:55 +0530

    patient-registration test file

---
+++ src/features/registration/screens/patient-registration/patient-sample.screen.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import {PatientSample} from '..';
+
+describe('PatientSample Screen', () => {
+  it('render patientSample correctly', () => {
+    const patientSample = render(<PatientSample />);
+    expect(patientSample).toMatchSnapshot();
+  });
+});
