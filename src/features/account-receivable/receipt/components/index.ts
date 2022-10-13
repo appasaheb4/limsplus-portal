1aaf9b030ddf2b32d0b91fbbd6d93ccbfd70423c
appasaheb4 <onlyappasaheb4@gmail.com>
2022-10-13 22:35:20 +0530

    receipt download

--- src/features/account-receivable/receipt/components/index.ts
+++ src/features/account-receivable/receipt/components/index.ts
@@ -1,3 +1,4 @@
 export * from './molecules/receipt-list.components';
 export * from './molecules/pdf-receipt.component';
 export * from './molecules/pdf-table-transaction-line.component';
+export * from './molecules/modal-receipt-share/modal-receipt-share.component';
