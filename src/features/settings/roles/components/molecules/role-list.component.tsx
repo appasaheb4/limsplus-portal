33fe0d857ff5d45200ffecca71e96616791ee2d7
Malav Naagar <73930050+malav4all@users.noreply.github.com>
2022-10-31 11:27:29 +0530

    settings tooltip issue fixed

--- src/features/settings/roles/components/molecules/role-list.component.tsx
+++ src/features/settings/roles/components/molecules/role-list.component.tsx
@@ -111,7 +111,7 @@ export const RoleList = (props: RoleListProps) => {
           formatter: (cellContent, row) => (
             <>
               <div className='flex flex-row'>
-                <Tooltip tooltipText='Delete' position='top'>
+                <Tooltip tooltipText='Delete'>
                   <Icons.IconContext
                     color='#fff'
                     size='20'
@@ -132,9 +132,14 @@ export const RoleList = (props: RoleListProps) => {
               </div>
             </>
           ),
-          headerClasses: 'sticky right-0  bg-gray-500 text-white',
+          headerClasses: 'sticky right-0  bg-gray-500 text-white z-50',
           classes: (cell, row, rowIndex, colIndex) => {
             return 'sticky right-0 bg-gray-500';
+          },
+          style: (cell, row, rowIndex, colIndex) => {
+            return {
+              zIndex: props.data?.length - rowIndex,
+            };
           },
         },
       ]}
