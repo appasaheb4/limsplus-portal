89895ca4017d197aff9b4cadeea197c5c842565b
appasaheb4 <onlyappasaheb4@gmail.com>
2022-11-14 12:08:29 +0530

    result update

--- src/features/master/master-analyte/screens/__tests__/master-analyte.screen.test.tsx
+++ src/features/master/master-analyte/screens/__tests__/master-analyte.screen.test.tsx
@@ -1,6 +1,7 @@
+//eslint-disable-next-line folders/match-regex
 import React from 'react';
 import {render} from '@testing-library/react';
-import {MasterAnalyte} from '.';
+import {MasterAnalyte} from '..';
 
 describe('MasterAnalyte Screen', () => {
   it('render masterAnalyte correctly', () => {
