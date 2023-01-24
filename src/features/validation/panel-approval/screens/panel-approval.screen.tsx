e35765decea9a557b5c9aa1207467c3dcdc40bab
appasaheb4 <onlyappasaheb4@gmail.com>
2023-01-24 18:02:14 +0530

    panel approval

--- src/features/validation/panel-approval/screens/panel-approval.screen.tsx
+++ src/features/validation/panel-approval/screens/panel-approval.screen.tsx
@@ -10,7 +10,7 @@ import {
 } from '@/library/components';
 import {useForm, Controller} from 'react-hook-form';
 import {RouterFlow} from '@/flows';
-import {PendingPanelApprovalList} from '../components';
+import {PendingPanelApprovalList, ResultList} from '../components';
 import '@/library/assets/css/accordion.css';
 import {useStores} from '@/stores';
 import 'react-accessible-accordion/dist/fancy-example.css';
@@ -39,6 +39,47 @@ const PanelApproval = observer(() => {
         <PageHeading title={routerStore.selectedComponents?.title || ''} />
         <PageHeadingLabDetails store={loginStore} />
       </Header>
+      <div className='p-3 rounded-lg shadow-xl overflow-auto'>
+        <span className='font-bold text-lg underline'>Result</span>
+        <ResultList
+          data={transactionDetailsStore.transactionListList || []}
+          totalSize={transactionDetailsStore.transactionListListCount}
+          isDelete={RouterFlow.checkPermission(
+            routerStore.userPermission,
+            'Delete',
+          )}
+          isEditModify={RouterFlow.checkPermission(
+            routerStore.userPermission,
+            'Edit/Modify',
+          )}
+          onSelectedRow={rows => {
+            setModalConfirm({
+              show: true,
+              type: 'delete',
+              id: rows,
+              title: 'Are you sure?',
+              body: 'Delete selected items!',
+            });
+          }}
+          onUpdateItem={(value: any, dataField: string, id: string) => {
+            setModalConfirm({
+              show: true,
+              type: 'update',
+              data: {value, dataField, id},
+              title: 'Are you sure?',
+              body: 'Update items!',
+            });
+          }}
+          onPageSizeChange={(page, limit) => {
+            // bannerStore.fetchListBanner(page, limit);
+          }}
+          onFilter={(type, filter, page, limit) => {
+            // bannerStore.BannerService.filter({
+            //   input: {type, filter, page, limit},
+            // });
+          }}
+        />
+      </div>
       <div className='p-1 rounded-lg shadow-xl overflow-auto'>
         <span className='font-bold text-lg underline'>
           Pending Panel Approval
@@ -99,48 +140,6 @@ const PanelApproval = observer(() => {
         <span className='font-bold text-lg underline'>
           Patient Demographics
         </span>
-        {/* <TransactionLineList
-          data={transactionDetailsStore.transactionListList || []}
-          totalSize={transactionDetailsStore.transactionListListCount}
-          isDelete={RouterFlow.checkPermission(
-            routerStore.userPermission,
-            'Delete',
-          )}
-          isEditModify={RouterFlow.checkPermission(
-            routerStore.userPermission,
-            'Edit/Modify',
-          )}
-          onDelete={selectedItem => setModalConfirm(selectedItem)}
-          onSelectedRow={rows => {
-            setModalConfirm({
-              show: true,
-              type: 'delete',
-              id: rows,
-              title: 'Are you sure?',
-              body: 'Delete selected items!',
-            });
-          }}
-          onUpdateItem={(value: any, dataField: string, id: string) => {
-            setModalConfirm({
-              show: true,
-              type: 'update',
-              data: {value, dataField, id},
-              title: 'Are you sure?',
-              body: 'Update items!',
-            });
-          }}
-          onPageSizeChange={(page, limit) => {
-            // bannerStore.fetchListBanner(page, limit);
-          }}
-          onFilter={(type, filter, page, limit) => {
-            // bannerStore.BannerService.filter({
-            //   input: {type, filter, page, limit},
-            // });
-          }}
-        /> */}
-      </div>
-      <div className='p-3 rounded-lg shadow-xl overflow-auto'>
-        <span className='font-bold text-lg underline'>Result</span>
         {/* <TransactionLineList
           data={transactionDetailsStore.transactionListList || []}
           totalSize={transactionDetailsStore.transactionListListCount}
