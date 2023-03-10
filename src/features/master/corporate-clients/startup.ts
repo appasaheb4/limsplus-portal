5f48852542faa9d8a63714eb667dbee8afd8d83c
Malav Naagar <73930050+malav4all@users.noreply.github.com>
2023-03-10 16:38:49 +0530

    some changes

--- src/features/master/corporate-clients/startup.ts
+++ src/features/master/corporate-clients/startup.ts
@@ -1,7 +1,15 @@
+import {eventEmitter} from '@/core-utils';
 import {stores} from '@/stores';
 const startup = async () => {
-  stores.corporateClientsStore.fetchCorporateClients();
-  stores.corporateClientsStore.corporateClientsService.counterInvoiceAc();
+  setTimeout(() => {
+    stores.corporateClientsStore.fetchCorporateClients();
+    stores.corporateClientsStore.corporateClientsService.counterInvoiceAc();
+  }, 2000);
+};
+
+export const resetCorporateClient = () => {
+  stores.corporateClientsStore.reset();
+  eventEmitter.emit('reload', {});
 };
 
 export default startup;
