46eec31f0ca286a9fe2d2746ceab5e461abf0f78
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2023-04-23 18:24:38 +0530

    table read

--- src/features/communication/file-import-export/models/file-import-export.model.ts
+++ src/features/communication/file-import-export/models/file-import-export.model.ts
@@ -1,42 +1,11 @@
 export class FileImportExport {
   _id: string;
-  countryName: string;
-  labId: number;
-  registrationDate: Date;
-  clientCode: string;
-  clientName: string;
-  patientName: string;
-  age: number;
-  ageUnits: string;
-  sex: string;
-  testName: string;
-  testCode: string;
-  sample: string;
-  dueDate: Date;
-  reportDate: Date;
-  status: string;
-  pdfReport: any;
+  transferType: string;
   dateOfEntry: Date;
   lastUpdated: Date;
-
   constructor(rawData: {[key in string]: any}) {
     this._id = rawData._id;
-    this.countryName = rawData.countryName;
-    this.labId = rawData.labId;
-    this.registrationDate = rawData.registrationDate;
-    this.clientCode = rawData.clientCode;
-    this.clientName = rawData.clientName;
-    this.patientName = rawData.patientName;
-    this.age = rawData.age;
-    this.ageUnits = rawData.ageUnits;
-    this.sex = rawData.sex;
-    this.testName = rawData.testName;
-    this.testCode = rawData.testCode;
-    this.sample = rawData.sample;
-    this.dueDate = rawData.dueDate;
-    this.reportDate = rawData.reportDate;
-    this.status = rawData.status;
-    this.pdfReport = rawData.reportDate;
+    this.transferType = rawData.transferType;
     this.dateOfEntry = rawData.dateOfEntry;
     this.lastUpdated = rawData.lastUpdated;
   }
--- src/features/communication/file-import-export/stores/file-import-export.store.ts
+++ src/features/communication/file-import-export/stores/file-import-export.store.ts
@@ -3,14 +3,17 @@ import {FileImportExport} from '../models';
 import {FileImportExportService} from '../services';
 
 export class FileImportExportStore {
+  inputFileImportExportList!: FileImportExport[];
   fileImportExportList!: FileImportExport[];
   fileImportExportListCount: number;
 
   constructor() {
+    this.inputFileImportExportList = [];
     this.fileImportExportList = [];
     this.fileImportExportListCount = 0;
 
     makeObservable<FileImportExportStore, any>(this, {
+      inputFileImportExportList: observable,
       fileImportExportList: observable,
       fileImportExportListCount: observable,
 
@@ -21,6 +24,10 @@ export class FileImportExportStore {
 
   get fileImportExportService() {
     return new FileImportExportService();
+  }
+
+  updateInputFileImpExport(data) {
+    this.inputFileImportExportList = data;
   }
 
   updateFileImportExportList(res) {
