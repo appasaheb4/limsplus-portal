51d8d655cb12026abda3304ae1f15d3be4759af1
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2023-06-16 19:24:24 +0530

    gre ui modify

--- src/features/result-entry/general-result-entry/startup.ts
+++ src/features/result-entry/general-result-entry/startup.ts
@@ -2,18 +2,28 @@ import {stores} from '@/stores';
 const startup = async () => {
   //patient result
   if (stores.loginStore.login) {
-    stores.patientResultStore.patientResultService.listPatientResultNotAutoUpdate(
-      {
-        pLab: stores.loginStore.login?.lab,
-        finishResult: 'P',
-      },
-    );
     // filter for default user plab set
-    stores.generalResultEntryStore.updateFilterGeneralResEntry({
-      ...stores.generalResultEntryStore.filterGeneralResEntry,
-      pLab: stores.loginStore?.login?.lab,
-    });
-    stores.patientResultStore.patientResultService.getPatientResultDistinct();
+
+    stores.patientResultStore.patientResultService
+      .getPatientResultDistinct()
+      .then(res => {
+        const pLab = stores.loginStore?.login?.lab;
+        const testCode =
+          res.getPatientResultDistinct.patientResultList[0]?._id?.testCode;
+
+        stores.generalResultEntryStore.updateFilterGeneralResEntry({
+          ...stores.generalResultEntryStore.filterGeneralResEntry,
+          pLab,
+          testCode,
+        });
+        stores.patientResultStore.patientResultService.listPatientResultNotAutoUpdate(
+          {
+            pLab,
+            testCode,
+            finishResult: 'P',
+          },
+        );
+      });
   }
 };
 
