fe3cd2d641148a7c8ac385531aa78151288529b8
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2023-06-27 10:59:09 +0530

    all code pushed.

--- src/library/components/atoms/icons/icons.component.tsx
+++ src/library/components/atoms/icons/icons.component.tsx
@@ -245,15 +245,20 @@ interface RIconProps {
 }
 
 export const RIcon = ({nameIcon, propsIcon}: RIconProps): JSX.Element => {
-  const lib = nameIcon
-    .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
-    .split(' ')[0]
-    .toLocaleLowerCase();
-  const ElementIcon: any = loadable(
-    () => import(`react-icons/${lib}/index.js`),
-    {
-      resolveComponent: (el: JSX.Element) => el[nameIcon as keyof JSX.Element],
-    },
-  );
-  return <ElementIcon {...propsIcon} />;
+  try {
+    const lib = nameIcon
+      .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+      .split(' ')[0]
+      .toLocaleLowerCase();
+    const ElementIcon: any = loadable(
+      () => import(`react-icons/${lib}/index.js`),
+      {
+        resolveComponent: el =>
+          el[nameIcon] != null ? el[nameIcon] : el[Object.keys(el.default)[0]],
+      },
+    );
+    return <ElementIcon {...propsIcon} />;
+  } catch (error) {
+    return <></>;
+  }
 };
