0a0dd1064b086fb07ad1c0993153e5a2b4a25a72
Malav <malav.naagar@imzcorporate.com>
2024-01-24 18:09:48 +0530

    add new images in sidebar

--- src/layouts/components/navbar.component.tsx
+++ src/layouts/components/navbar.component.tsx
@@ -57,7 +57,6 @@ const NavbarComponent = observer(({ dispatch, sidebar }) => {
           backgroundColor: appStore.applicationSetting?.navBarColor,
           backgroundImage: `url(${appStore.applicationSetting?.navbarImage})`,
           backgroundSize: '100% 100%',
-          backgroundRepeat: 'no-repeat',
           display: 'flex',
         }}
         light
