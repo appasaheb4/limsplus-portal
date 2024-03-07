ccab9d4fccd50055252b3be4fe64e2e3de88c6e9
Malav <malav.naagar@imzcorporate.com>
2024-03-07 15:47:19 +0530

    some chnage

--- src/features/settings/roles/components/molecules/role-list.component.tsx
+++ src/features/settings/roles/components/molecules/role-list.component.tsx
@@ -77,6 +77,18 @@ export const RoleList = (props: RoleListProps) => {
           headerStyle: {
             fontSize: 0,
           },
+          formatter: (cellContent, row) => {
+            const maxLength = 5;
+            const displayTestName =
+              row.description.length > maxLength
+                ? row.description.slice(0, Math.max(0, maxLength)) + '...'
+                : row.description;
+            return (
+              <div className='flex flex-row'>
+                <span title={row.description}>{`${displayTestName}`}</span>
+              </div>
+            );
+          },
           headerClasses: 'textHeader',
           sortCaret: (order, column) => sortCaret(order, column),
           filter: textFilter({
