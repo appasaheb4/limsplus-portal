85af7edda891c83847bfefd23e2db44a86578054
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-04-02 17:48:03 +0530

    duplicate codes remove

--- src/features/registration/patient-registration/components/orgransims/auto-complete-filter-single-select-state-collection-center.component.tsx
+++ src/features/registration/patient-registration/components/orgransims/auto-complete-filter-single-select-state-collection-center.component.tsx
@@ -1,16 +1,16 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {Spinner} from 'react-bootstrap';
-import {observer} from 'mobx-react';
-import {useStores} from '@/stores';
-import {Icons} from '@/library/components';
+import React, { useState, useEffect, useRef } from 'react';
+import { Spinner } from 'react-bootstrap';
+import { observer } from 'mobx-react';
+import { useStores } from '@/stores';
+import { Icons } from '@/library/components';
 
 interface AutoCompleteFilterSingleSelectCollectionCenterProps {
   onSelect: (item: any) => void;
 }
 
 export const AutoCompleteFilterSingleSelectCollectionCenter = observer(
-  ({onSelect}: AutoCompleteFilterSingleSelectCollectionCenterProps) => {
-    const {loading, registrationLocationsStore} = useStores();
+  ({ onSelect }: AutoCompleteFilterSingleSelectCollectionCenterProps) => {
+    const { loading, registrationLocationsStore } = useStores();
     const [value, setValue] = useState<string>('');
     const [options, setOptions] = useState<any[]>();
     const [isListOpen, setIsListOpen] = useState<boolean>(false);
@@ -39,7 +39,11 @@ export const AutoCompleteFilterSingleSelectCollectionCenter = observer(
     useOutsideAlerter(wrapperRef);
 
     useEffect(() => {
-      setOptions(registrationLocationsStore.listRegistrationLocations);
+      setOptions(
+        registrationLocationsStore.listRegistrationLocations?.filter(
+          item => item.status == 'A',
+        ),
+      );
     }, [registrationLocationsStore.listRegistrationLocations]);
 
     const onFilter = (value: string) => {
