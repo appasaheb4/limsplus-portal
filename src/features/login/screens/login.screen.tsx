488f0fffdd42df3c4ff9e7ebea8fbe3e469b9928
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-04-08 09:59:23 +0530

    login webportal changed

--- src/features/login/screens/login.screen.tsx
+++ src/features/login/screens/login.screen.tsx
@@ -336,7 +336,7 @@ export const Login = observer(() => {
                                           ? 'https://www.limsplussolutions.com'
                                           : window.location.origin,
                                       // webPortal:
-                                      //   'https://config.limsplussolutions.com',
+                                      //   'https://demo.limsplussolutions.com',
                                     },
                                   }).then(async res => {
                                     if (res.checkUserExitsUserId?.success) {
