cefe6556147fbf36537c0b4882c1e33dec78f978
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-05-01 12:41:47 +0530

    doctor and role bus fixed.

--- src/features/settings/roles/models/role.model.ts
+++ src/features/settings/roles/models/role.model.ts
@@ -1,9 +1,9 @@
 export class Role {
-  code?: string | undefined;
+  code?: string;
+  description?: string;
   status?: string;
-  description?: string | undefined;
-  environment?: string | undefined;
-  constructor(rawData: {[key in string]: any}) {
+  environment?: string;
+  constructor(rawData: { [key in string]: any }) {
     this.code = rawData.code;
     this.status = rawData.status;
     this.description = rawData.description;
