43a88ad441f1ccd0847eec4b5d191a915df8bca3
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-05-18 10:04:07 +0530

    login url detect

--- src/features/login/screens/login.screen.tsx
+++ src/features/login/screens/login.screen.tsx
@@ -334,7 +334,7 @@ export const Login = observer(() => {
                                       webPortal:
                                         process.env.REACT_APP_ENV === 'Local'
                                           ? 'https://www.limsplussolutions.com'
-                                          : 'https://www.limsplussolutions.com',
+                                          : window.location.origin,
                                       // webPortal:
                                       //   'https://limsplussolutions.com',
                                     },
