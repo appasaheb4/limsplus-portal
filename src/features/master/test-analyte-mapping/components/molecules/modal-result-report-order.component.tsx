32b7226b9c9e588f58eb9636a2326e8c2f91387b
malav <malav.naagar@imzcorporate.com>
2024-06-12 12:05:01 +0530

    test anlayte mapping modal issue fixed

--- src/features/master/test-analyte-mapping/components/molecules/modal-result-report-order.component.tsx
+++ src/features/master/test-analyte-mapping/components/molecules/modal-result-report-order.component.tsx
@@ -96,7 +96,7 @@ export const ModalResultReportOrder = observer(
                   </div>
                   {/*body*/}
 
-                  <div className='relative p-2 flex-auto'>
+                  <div className='relative p-2 flex-auto max-h-[60vh] overflow-y-scroll'>
                     <Table striped bordered className='max-h-5' size='sm'>
                       <thead>
                         <tr className='text-xs'>
