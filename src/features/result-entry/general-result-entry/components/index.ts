c3f045670c6c147526ad8453ab994d3bd8daad63
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2024-08-24 16:47:50 +0530

    gre old ui

--- src/features/result-entry/general-result-entry/components/index.ts
+++ src/features/result-entry/general-result-entry/components/index.ts
@@ -1,3 +1,3 @@
 export { FilterInputTable } from './molecules/input/filter-input-table.component';
-export { GeneralResultEntryList } from './molecules/output/general-result-entry-list.component';
+export { GeneralResultEntryList } from './molecules/output1/general-result-entry-list.component';
 export { ModalPatientDemographics } from './molecules/patient-demographics/modal-patient-demographics.component';
